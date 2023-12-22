a115628fcea936527fccf9bf725c1f3ac4672547
Mikias Daniel <beldanmik@gmail.com>
2023-12-22 12:08:04 +0300

    idk some changes i guess

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,13 +17,13 @@ export class HeroesComponent {
     this.messageService.add(`Selected hero with id : ${hero.id} `);//back ticks for using the value of the hero.id variable using the ${} variable access method
 
   }*/
-  constructor(private heroService : HeroService, private messageService : MessageService, public location : Location){//declaring instances for all the services we need to use
+  constructor(private heroService : HeroService, private messageService : MessageService, public location : Location){//declaring instances for all the services so we could be able to use them
 
   }
   getHeroes() : void{
     this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);//the subscribe method accepts the values emitted by an observable, in this case from the getHeroes() method after getting the heroes observable using the callback function inside the subscribe method we assign the values to this.heroes property
   }
-  ngOnInit(): void{
+  ngOnInit(): void{//this method automatically executes on initialization.
     this.getHeroes();//for accessing the getHeroes() method on component initialization, lifecycle hook(2nd in precedence, comes next to ngOnChanges())
   }
   goBack():void{
